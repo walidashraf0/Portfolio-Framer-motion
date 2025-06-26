3f255b6225625c65c2ca0d84de4bfee2576e7ec4
Walid Ashraf <walidashraf050@gmail.com>
2025-06-27 02:52:25 +0300

    Fourth & Fifth Sections

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -59,7 +59,7 @@ const Body = () => {
         <div className="flex justify-between items-center py-[42px] px-[24px]">
           <div className="flex gap-7 max-md:flex-col max-md:justify-center">
             <div className="flex flex-col gap-2 items-center justify-center">
-              <img src="/4.png" alt="" className="w-30" />
+              <img src="/4.png" alt="" className="w-30 mb-2.5" />
               <h2 className="font-semibold text-xl text-black">
                 Product Design
               </h2>
@@ -71,7 +71,7 @@ const Body = () => {
               </p>
             </div>
             <div className="flex flex-col gap-2 items-center justify-center">
-              <img src="/5.png" alt="" className="w-30" />
+              <img src="/5.png" alt="" className="w-30 mb-2.5" />
               <h2 className="font-semibold text-xl text-black">
                 Visual Design
               </h2>
@@ -83,7 +83,7 @@ const Body = () => {
               </p>
             </div>
             <div className="flex flex-col gap-2 items-center justify-center">
-              <img src="/6.png" alt="" className="w-30" />
+              <img src="/6.png" alt="" className="w-30 mb-2.5" />
               <h2 className="font-semibold text-xl text-black">
                 Art Direction
               </h2>
@@ -99,7 +99,155 @@ const Body = () => {
       </section>
 
       {/* Fourth Section */}
-      
+      <section className="w-full">
+        <div className="w-full grid grid-cols-2 gap-10 max-md:grid-cols-1 py-[42px] px-[24px]">
+          <div className="flex gap-5 items-center justify-center">
+            <img src="/4.png" alt="" className="w-30" />
+            <div className="flex flex-col items-start justify-center gap-4">
+              <h2 className="font-semibold text-xl text-black">
+                Product Design
+              </h2>
+              <p className="text-sm text-gray-500">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Voluptatem distinctio sint blanditiis ipsum totam obcaecati
+                dicta error facere, explicabo qui atque rerum, nobis eos!
+                Aliquid quo distinctio accusantium quidem voluptatem.
+              </p>
+            </div>
+          </div>
+          <div className="flex gap-5 items-center justify-center">
+            <img src="/5.png" alt="" className="w-30" />
+            <div className="flex flex-col items-start justify-center gap-4">
+              <h2 className="font-semibold text-xl text-black">
+                Visual Design
+              </h2>
+              <p className="text-sm text-gray-500">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Voluptatem distinctio sint blanditiis ipsum totam obcaecati
+                dicta error facere, explicabo qui atque rerum, nobis eos!
+                Aliquid quo distinctio accusantium quidem voluptatem.
+              </p>
+            </div>
+          </div>
+          <div className="flex gap-5 items-center justify-center">
+            <img src="/6.png" alt="" className="w-30" />
+            <div className="flex flex-col items-start justify-center gap-4">
+              <h2 className="font-semibold text-xl text-black">
+                Art Direction
+              </h2>
+              <p className="text-sm text-gray-500">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Voluptatem distinctio sint blanditiis ipsum totam obcaecati
+                dicta error facere, explicabo qui atque rerum, nobis eos!
+                Aliquid quo distinctio accusantium quidem voluptatem.
+              </p>
+            </div>
+          </div>
+          <div className="flex gap-5 items-center justify-center">
+            <img src="/9.png" alt="" className="w-30" />
+            <div className="flex flex-col items-start justify-center gap-4">
+              <h2 className="font-semibold text-xl text-black">
+                Art Direction
+              </h2>
+              <p className="text-sm text-gray-500">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Voluptatem distinctio sint blanditiis ipsum totam obcaecati
+                dicta error facere, explicabo qui atque rerum, nobis eos!
+                Aliquid quo distinctio accusantium quidem voluptatem.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Fifth Section */}
+      <section className="w-full py-6">
+        <div className="py-5">
+          <h1 className="text-4xl font-semibold text-center">Latest Work</h1>
+        </div>
+        <div className="w-full grid grid-cols-3 gap-10 max-md:grid-cols-1 max-md:text-center max-md:justify-center max-md:items-center py-[42px] px-[24px]">
+          <div className="flex flex-col gap-3 items-center justify-center">
+            <img src="/10.jpeg" alt="" className="w-[400px]" />
+            <div className="flex flex-col gap-2">
+              <h2 className="font-semibold text-xl text-black">
+                Product Design
+              </h2>
+              <p className="text-sm text-gray-500">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Voluptatem distinctio sint blanditiis ipsum totam obcaecati
+                dicta error facere, explicabo qui atque rerum, nobis eos!
+                Aliquid quo distinctio accusantium quidem voluptatem.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-3 items-center justify-center">
+            <img src="/10.jpeg" alt="" className="w-[400px]" />
+            <div className="flex flex-col gap-2">
+              <h2 className="font-semibold text-xl text-black">
+                Visual Design
+              </h2>
+              <p className="text-sm text-gray-500">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Voluptatem distinctio sint blanditiis ipsum totam obcaecati
+                dicta error facere, explicabo qui atque rerum, nobis eos!
+                Aliquid quo distinctio accusantium quidem voluptatem.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-3 items-center justify-center">
+            <img src="/11.jpeg" alt="" className="w-[400px]" />
+            <div className="flex flex-col gap-2">
+              <h2 className="font-semibold text-xl text-black">Art Design</h2>
+              <p className="text-sm text-gray-500">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Voluptatem distinctio sint blanditiis ipsum totam obcaecati
+                dicta error facere, explicabo qui atque rerum, nobis eos!
+                Aliquid quo distinctio accusantium quidem voluptatem.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-3 items-center justify-center">
+            <img src="/12.jpeg" alt="" className="w-[400px]" />
+            <div className="flex flex-col gap-2">
+              <h2 className="font-semibold text-xl text-black">
+                Product Design
+              </h2>
+              <p className="text-sm text-gray-500">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Voluptatem distinctio sint blanditiis ipsum totam obcaecati
+                dicta error facere, explicabo qui atque rerum, nobis eos!
+                Aliquid quo distinctio accusantium quidem voluptatem.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-3 items-center justify-center">
+            <img src="/13.jpeg" alt="" className="w-[400px]" />
+            <div className="flex flex-col gap-2">
+              <h2 className="font-semibold text-xl text-black">
+                Visual Design
+              </h2>
+              <p className="text-sm text-gray-500">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Voluptatem distinctio sint blanditiis ipsum totam obcaecati
+                dicta error facere, explicabo qui atque rerum, nobis eos!
+                Aliquid quo distinctio accusantium quidem voluptatem.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-3 items-center justify-center">
+            <img src="/14.jpeg" alt="" className="w-[400px]" />
+            <div className="flex flex-col gap-2">
+              <h2 className="font-semibold text-xl text-black">Art Design</h2>
+              <p className="text-sm text-gray-500">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Voluptatem distinctio sint blanditiis ipsum totam obcaecati
+                dicta error facere, explicabo qui atque rerum, nobis eos!
+                Aliquid quo distinctio accusantium quidem voluptatem.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
